09e04c39dbb4c78a5d339f7ff887ee4f4008bfa6
Philip Eskins <philipeskins@gmail.com>
2020-08-02 16:41:01 -0700

    keyless car

---
+++ javascript/keyless.js
@@ -0,0 +1,9 @@
+const age = prompt("Enter your age");
+
+if (age > 18) {
+  alert("Powering On. Enjoy the ride!")
+} else if (age < 18) {
+  alert("Sorry, you are too young to drive this car. Powering off");
+} else {
+  alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
