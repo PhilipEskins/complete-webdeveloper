8144de7e953d84950c466c3aefca9560243f65ed
Philip Eskins <philipeskins@gmail.com>
2020-08-01 20:15:59 -0700

    better calculator

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,18 @@
 var first = prompt("Enter first number");
-var second = prompt("Enter second number");
-var total = Number(first) + Number(second);
-var sum = alert("The sum is: " + total);
+if (isNaN(first) === true) {
+  alert("That is not a number");
+} else {
+  var second = prompt("Enter second number");
+  if (isNaN(second) === true) {
+    alert("That is not a number");
+  } else {
+    var operator = prompt("Enter operator (ie +, -, *, /, %)");
+    if (operator === "+" || operator === "-" || operator === "*" || operator === "/" || operator === "%") {
+      var total = eval(first + operator + second);
+      var sum = alert("The sum is: " + total);
+    } else {
+      alert("You didn't enter a valid operator");
+    }
+  }
+}
+// var total = Number(first) + operator + Number(second);
