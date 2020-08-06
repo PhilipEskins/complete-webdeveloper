8b9c1bec7582fc81ee841cb41eee9fd43a5e0c94
Philip Eskins <philipeskins@gmail.com>
2020-08-05 18:56:59 -0700

    facebook commit

---
+++ facebook/scripts.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+let database = [{
+  username: "Mike",
+  password: "Indie",
+}];
+
+let newsfeed = [{
+  username: "Mike",
+  timeline: "Yesterday"
+}, {
+  username: "Mike",
+  timeline: "Today"
+}, {
+  username: "Mike",
+  timeline: "Tomorrow"
+}];
+
+console.log(database, newsfeed)
