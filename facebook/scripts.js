7132a7a8ca931f9624701f8a47260e0a6bac6117
Philip Eskins <philipeskins@gmail.com>
2020-08-06 19:26:24 -0700

    loops

--- facebook/scripts.js
+++ facebook/scripts.js
@@ -1,7 +1,15 @@
 var database = [
   {
-    username: "Andrei",
+    username: "andrei",
     password: "supersecret"
+  },
+  {
+    username: "sally",
+    password: "123"
+  },
+  {
+    username: "ingrid",
+    password: "777"
   }
 ];
 
@@ -13,14 +21,27 @@ var newsfeed = [
   {
     username: "Sally",
     timeline: "Javascript is so cool"
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is pretty cool!"
   }
 ];
 
 var userNamePrompt = prompt("Whats your username?");
 var passwordPrompt = prompt("Whats your password?");
 
-function signIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
+function isUserValid(username, password) {
+  for(var i=0; i < database.length; i++) {
+    if (database[i].username === username && database[i].password === password) {
+    return true;
+    }
+  }
+  return false;
+}
+
+function signIn(username, password) {
+  if (isUserValid(username, password)) {
     console.log(newsfeed);
   } else {
     console.log("Sorry wrong username or password");
