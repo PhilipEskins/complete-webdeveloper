9bdbae6cc7b3f6799328e2a4f4c2f45684dcdf60
Philip Eskins <philipeskins@gmail.com>
2020-08-16 19:36:05 -0700

    dom selectors

---
+++ javascript/domselectors.js
@@ -0,0 +1,8 @@
+let selector = document.querySelector("h1");
+console.log(selector);
+
+let className = document.getElementsByClassName("second");
+console.log(className);
+
+let elementId = document.getElementById("first");
+console.log(elementId)
