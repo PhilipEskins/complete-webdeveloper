daa8febbec0211e5269256e2d7ab536c59d4f553
Philip Eskins <philipeskins@gmail.com>
2020-08-18 18:51:52 -0700

    added new function for completing an item

--- javascript/shopping.js
+++ javascript/shopping.js
@@ -1,9 +1,17 @@
 let button = document.getElementById('enter');
 let input = document.getElementById('userInput');
 
+//function to add event listener to existing li elements
 function addClick() {
   let li = document.getElementsByTagName("li");
   for(q = 0; q < li.length; q++) {
-    li[q].addEventListener("click", (e) => e.target.classList.toggle("done"));
+    complete(li[q]);
   }
 }
+
+//function to toggle the class done on each item
+function complete(item) {
+  item.addEventListener("click", (e) => e.target.classList.toggle("done"));
+}
+
+addClick();
