cb6b57362f92a3f01fdbc8ec6cdb950b2d8d560c
Philip Eskins <philipeskins@gmail.com>
2020-08-21 19:48:09 -0700

    dom events

--- javascript/shopping.js
+++ javascript/shopping.js
@@ -1,5 +1,34 @@
 let button = document.getElementById('enter');
 let input = document.getElementById('userInput');
+var ul = document.querySelector("ul");
+
+function inputLength() {
+  return input.value.length
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  complete(li);
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(e) {
+  if (inputLength() > 0  && e.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
 
 //function to add event listener to existing li elements
 function addClick() {
